f54452d9f67e81dc74165a2f0e9e4a5e1162874f
Sanchezmo <psmoran87@gmail.com>
2023-05-11 22:12:05 +0200

    Premium funtions working

--- bd/Recetas.sql
+++ bd/Recetas.sql
@@ -1,27 +1,20 @@
 
 CREATE DATABASE IF NOT EXISTS Recetas;
 USE Recetas;
+
+DROP TABLE IF EXISTS Coments;
 DROP TABLE IF EXISTS Recipes;
+DROP TABLE IF EXISTS LogrosUser;
 DROP TABLE IF EXISTS Categories;
+DROP TABLE IF EXISTS Media;
+DROP TABLE IF EXISTS Logros;
 DROP TABLE IF EXISTS Users;
-CREATE TABLE Categories
-(      
+
+
+CREATE TABLE Categories(      
     CategoryID INTEGER PRIMARY KEY AUTO_INCREMENT,
     CategoryName VARCHAR(50),
     Description VARCHAR(255)
-);
-CREATE TABLE Recipes(
-    RecipeID INTEGER PRIMARY KEY AUTO_INCREMENT,
-    RecipeName VARCHAR(50),
-    Recipe VARCHAR(255),
-    Imagen VARCHAR(100),
-    Extension VARCHAR(30),
-    CategoryID INTEGER,
-    OwnerID INTEGER,
-    Premium VARCHAR(2),
-    votos INT(10),
-	FOREIGN KEY (CategoryID) REFERENCES Categories (CategoryID)ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (OwnerID) REFERENCES Users (CustomerID)ON DELETE CASCADE ON UPDATE CASCADE
 );
 CREATE TABLE Users(      
     CustomerID INTEGER PRIMARY KEY AUTO_INCREMENT,
@@ -32,8 +25,61 @@ CREATE TABLE Users(
     Pass VARCHAR(25)
     
 );
+CREATE TABLE Logros(      
+    LogroID INTEGER PRIMARY KEY AUTO_INCREMENT,
+    Logro VARCHAR(100),
+    LogroDescription VARCHAR(250)
+);
+CREATE TABLE Media(
+    MediaID INTEGER PRIMARY KEY AUTO_INCREMENT,
+    MediaPath VARCHAR(250),
+    MediaSize INTEGER,
+    MediaName VARCHAR(100),
+    Extension VARCHAR(100),
+    OwnerID INTEGER,
+    FOREIGN KEY (OwnerID) REFERENCES Users (CustomerID)ON DELETE CASCADE ON UPDATE CASCADE
+);
+CREATE TABLE Recipes(
+    RecipeID INTEGER PRIMARY KEY AUTO_INCREMENT,
+    RecipeName VARCHAR(50),
+    Recipe VARCHAR(255),
+    MediaID INTEGER,
+    CategoryID INTEGER,
+    OwnerID INTEGER,
+    Premium VARCHAR(2),
+    Vote INT(10),
+	FOREIGN KEY (CategoryID) REFERENCES Categories (CategoryID)ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (MediaID) REFERENCES Media (MediaID)ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (OwnerID) REFERENCES Users (CustomerID)ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE Coments(      
+    ComentID INTEGER PRIMARY KEY AUTO_INCREMENT,
+    Coment VARCHAR(50),
+    OwnerID INTEGER,
+    RecipeID INTEGER,
+    FOREIGN KEY (RecipeID) REFERENCES Recipes (RecipeID)ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (OwnerID) REFERENCES Users (CustomerID)ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE LogrosUser(      
+    LogroUserID INTEGER PRIMARY KEY AUTO_INCREMENT,
+    OwnerID INTEGER,
+    LogroID INTEGER,
+    FOREIGN KEY (OwnerID) REFERENCES Users (CustomerID)ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (LogroID) REFERENCES Logros (LogroID)ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
 INSERT INTO Users (CustomerName, Email, Pass,Admin,Premium) VALUES("admin", "admin@admin.com", "admin","SI","SI");
 INSERT INTO Users (CustomerName, Email, Pass,Admin,Premium) VALUES("user", "user@admin.com", "user","NO","NO");
 INSERT INTO Users (CustomerName, Email, Pass,Admin,Premium) VALUES("userpremium", "userpremium@admin.com", "user","NO","SI");
 INSERT INTO Categories (CategoryName, Description) VALUES("Cocina Española","Platos tipicos de España");
-INSERT INTO Recipes (RecipeName,Recipe,Imagen,Extension,CategoryID,Premium)VALUES ("Tortilla de patata","Se frien patatas y luego se baten huevos , se mezcla y a la sarten","vacio","vacio",1,"NO");
+INSERT INTO Media(MediaPath ,MediaSize,MediaName, Extension,OwnerID) VALUES("../uploadssalchicha.jpg",15555000,"uploadssalchicha","jpg",1);
+INSERT INTO Recipes (RecipeName,Recipe,MediaID,CategoryID,Premium,OwnerID,Vote)VALUES ("Tortilla de patata","Se frien patatas y luego se baten huevos , se mezcla y a la sarten",1,1,"NO",1,0);
+INSERT INTO Logros (Logro,LogroDescription)VALUES("Participativo","Participa aportando 3 recetas");
+INSERT INTO Logros (Logro,LogroDescription)VALUES(" Muy Participativo","Participa aportando 10 recetas");
+INSERT INTO Logros (Logro,LogroDescription)VALUES("Dios culinario","Participa aportando 50 recetas");
+INSERT INTO Logros (Logro,LogroDescription)VALUES("Buen aporte","Recibe 1 voto con tu receta");
+INSERT INTO Logros (Logro,LogroDescription)VALUES("Muy rico","Recibe 5 voto con tu receta");
+INSERT INTO Logros (Logro,LogroDescription)VALUES("Tu sí que vales","Recibe 50 voto con tu receta");
--- register.php
+++ register.php
@@ -1,75 +1,98 @@
 <?php include('./includes/header.php');  session_start();?>
-<?php include('./includes/navbar.php'); ?>
+<?php include('./includes/navbar.php'); 
+
+if(isset($_POST['user'])&& isset($_POST['email'])&& isset($_POST['password'])&& isset($_POST['password2'])&& isset($_POST['terminos'])){
+  if($_POST['password']==$_POST['password2']){
+    $_SESSION['login']="OK";
+  header("Location: login.php");
+   
+
+  }
+  $_SESSION['mensaje2']="Las password deben coincidir";
+    
+  
+}
+if(isset($_POST['user'])&& isset($_POST['email'])&& isset($_POST['password'])&& isset($_POST['password2'])&& !isset($_POST['terminos'])){
+  $_SESSION['mensaje2']="Acepte los terminos de uso";
+  
+}
+
+?>
 <section class="vh-100" style="background-color: #eee;">
-  <div class="container h-100">
-    <div class="row d-flex justify-content-center align-items-center h-100">
-      <div class="col-lg-12 col-xl-11">
-        <div class="card text-black" style="border-radius: 25px;">
-          <div class="card-body p-md-5">
-            <div class="row justify-content-center">
-              <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+    <div class="container h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-lg-12 col-xl-11">
+                <div class="card text-black" style="border-radius: 25px;">
+                    <div class="card-body p-md-5">
+                        <div class="row justify-content-center">
+                            <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
 
-                <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Registro</p>
+                                <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Registro</p>
+                                
+                                <form class="mx-1 mx-md-4" method="post" action="register.php" enctype="multipart/form-data">
+                                    <div class="d-flex flex-row align-items-center mb-4">
+                                        <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                                        <div class="form-outline flex-fill mb-0">
+                                            <label class="form-label" for="form3Example1c">Nombre</label>
+                                            <input type="text" id="form3Example1c" name="user" class="form-control" />
 
-                <form class="mx-1 mx-md-4">
+                                        </div>
+                                    </div>
 
-                  <div class="d-flex flex-row align-items-center mb-4">
-                    <i class="fas fa-user fa-lg me-3 fa-fw"></i>
-                    <div class="form-outline flex-fill mb-0">
-                      <input type="text" id="form3Example1c" class="form-control" />
-                      <label class="form-label" for="form3Example1c">Nombre</label>
+                                    <div class="d-flex flex-row align-items-center mb-4">
+                                        <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                                        <div class="form-outline flex-fill mb-0">
+                                        <label class="form-label" for="form3Example3c">Email</label>
+                                            <input type="email" id="form3Example3c" name="email"class="form-control" />
+                                            
+                                        </div>
+                                    </div>
+
+                                    <div class="d-flex flex-row align-items-center mb-4">
+                                        <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                                        <div class="form-outline flex-fill mb-0">
+                                        <label class="form-label" for="form3Example4c">Password</label>
+                                            <input type="password" id="form3Example4c" name="password" class="form-control" />
+                                           
+                                        </div>
+                                    </div>
+
+                                    <div class="d-flex flex-row align-items-center mb-4">
+                                        <i class="fas fa-key fa-lg me-3 fa-fw"></i>
+                                        <div class="form-outline flex-fill mb-0">
+                                        <label class="form-label" for="form3Example4cd">Repetir Password</label>
+                                            <input type="password" id="form3Example4cd" name="password2" class="form-control" />
+                                         <?php if($_SESSION['mensaje2']!=""){ ?>
+                                          <div class='alert alert-danger'><?php echo $_SESSION['mensaje2'] ?></div>
+                                         <?php session_unset(); } ?>   
+                                        </div>
+                                    </div>
+
+                                    <div class="form-check d-flex justify-content-center mb-5">
+                                        <input class="form-check-input me-2" name="terminos" type="checkbox" value="aceptados"
+                                            id="form2Example3c" />
+                                        <label class="form-check-label" for="form2Example3">
+                                            Estoy de acuerdo con los <a href="terminos.php">Terminos de uso</a>
+                                        </label>
+                                    </div>
+
+                                    <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                                        <button type="submit" class="btn btn-primary btn-lg">Registrar</button>
+                                    </div>
+
+                                </form>
+
+                            </div>
+                            <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+
+                                <img src="./img/chef.png" class="img-fluid" width="200%" alt="Sample image">
+
+                            </div>
+                        </div>
                     </div>
-                  </div>
-
-                  <div class="d-flex flex-row align-items-center mb-4">
-                    <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
-                    <div class="form-outline flex-fill mb-0">
-                      <input type="email" id="form3Example3c" class="form-control" />
-                      <label class="form-label" for="form3Example3c">Email</label>
-                    </div>
-                  </div>
-
-                  <div class="d-flex flex-row align-items-center mb-4">
-                    <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
-                    <div class="form-outline flex-fill mb-0">
-                      <input type="password" id="form3Example4c" class="form-control" />
-                      <label class="form-label" for="form3Example4c">Password</label>
-                    </div>
-                  </div>
-
-                  <div class="d-flex flex-row align-items-center mb-4">
-                    <i class="fas fa-key fa-lg me-3 fa-fw"></i>
-                    <div class="form-outline flex-fill mb-0">
-                      <input type="password" id="form3Example4cd" class="form-control" />
-                      <label class="form-label" for="form3Example4cd">Repetir Password</label>
-                    </div>
-                  </div>
-
-                  <div class="form-check d-flex justify-content-center mb-5">
-                    <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3c" />
-                    <label class="form-check-label" for="form2Example3">
-                      Estoy de acuerdo con los <a href="#!">Terminos de uso</a>
-                    </label>
-                  </div>
-
-                  <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                    <button type="button" class="btn btn-primary btn-lg">Registrar</button>
-                  </div>
-
-                </form>
-
-              </div>
-              <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-
-                <img src="../uploadssalchicha.jpg"
-                  class="img-fluid" alt="Sample image">
-
-              </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
     </div>
-  </div>
 </section>
 <?php include('./includes/footer.php'); ?>

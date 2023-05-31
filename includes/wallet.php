72ac4dc327ef248c13d391b9c9ec99232072be75
Sanchezmo <psmoran87@gmail.com>
2023-05-31 13:43:38 +0200

    Premium pays works

--- includes/wallet.php
+++ includes/wallet.php
@@ -1,19 +1,40 @@
-<div class="container">
+<div class="container-fluid">
     <div class="card">
         <div class="card-header" style="text-align:center;">
-            Wallet
+            Hazte Premium
         </div>
         <div class="card-body">
-            <h5 class="card-title"><button type="button" class="btn btn-success sm" id="conexion_wallet"
-                    onclick="conectarMetamask()">Conectar Metamask</button></h5>
-            
-            <p class="card-text" id="cuenta"></p>
-            <p class="card-text" id="balance"></p>
+            <h5 class="card-title"><button type="button" class="btn btn-info sm" id="conexion_wallet"
+                    onclick="conectarMetamask()">Conectar con Metamask</button></h5>
+
+
+            <form action="mysuscription.php" method="post" name="enviar">
+                <input type="hidden" name="cuenta" id="cuenta" value="">
+                <input type="hidden" name="balance" id="balance" value="">
+
+            </form>
+            <form action="mysuscription.php" method="post" name="enviarpremium">
+                <input type="hidden" name="premium" id="premium" value="">
+            </form>
+            <?php if(!empty($_POST['cuenta'])){ echo $_POST['cuenta']; echo "<p> saldo: ".$_POST['balance']."</p>";}?>
+
 
         </div>
+        <div class="card-footer"><button type="button" class="btn btn-success sm" id="conexion_wallet"
+                onclick="pagar()">Pagar Premium</button></div>
     </div>
 </div>
 <script>
+//window.ethereum.networkVersion == 10 para controlar los pagos sean en la mainnet.
+function pagar() {
+    ethereum.request({
+        method: 'eth_requestAccounts'
+    }).then(accounts => {
+        account = accounts[0];
+        transferETH(account, "0x937fbAD70a9Eeb01d645399031fCA95182308800", 0.01);
+    });
+}
+
 function conectarMetamask() {
     let account;
     ethereum.request({
@@ -22,7 +43,7 @@ function conectarMetamask() {
         account = accounts[0];
         console.log(account);
         let p = document.getElementById('cuenta');
-        p.innerHTML = "Cuenta: " + account;
+        p.value = "Cuenta: " + account;
         getBalance(account);
     });
 }
@@ -33,35 +54,38 @@ function getBalance(account) {
         method: 'eth_getBalance',
         params: [account, 'latest']
     }).then(result => {
-
         let wei = parseInt(result, 16);
         let balance = wei / (10 ** 18);
-
         let p2 = document.getElementById('balance');
-
-        p2.innerHTML = "Saldo: " + balance + " ETH";
-
+        p2.value = balance;
+        document.enviar.submit();
     });
-
 }
 
 function transferETH(from, to, value) {
-    let wei = "0x" + (parseInt(value) * (10 ** 18)).toString(16);
+    let wei = "0x" + (parseFloat(value) * (10 ** 18)).toString(16);
     let transactionParam = {
         to: to,
         from: from,
         value: wei
     };
-
     ethereum.request({
         method: 'eth_sendTransaction',
         params: [transactionParam]
     }).then(txhash => {
         console.log(txhash);
+        checkTransactionconfirmation(txhash)
+            .then(r => alert(r))
+            .then(() => {
+                let pre = document.getElementById('premium');
+                pre.value = "SI";
+                document.enviarpremium.submit();
+            })
+
+
     });
 }
-
-
+//window.ethereum.networkVersion == 10 para que sea la mainnet.
 
 function checkTransactionconfirmation(txhash) {
 
@@ -70,7 +94,7 @@ function checkTransactionconfirmation(txhash) {
             method: 'eth_getTransactionReceipt',
             params: [txhash]
         }).then(r => {
-            if (r != null) return 'confirmed';
+            if (r != null) return 'Pago Confirmado, vuelve a loguear para aplicar los cambios';
             else return checkTransactionLoop();
         });
     };
@@ -79,6 +103,7 @@ function checkTransactionconfirmation(txhash) {
 }
 
 function cambioCuenta(nuevacuenta) {
+
     conectarMetamask();
 
 }
